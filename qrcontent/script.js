eb227fe0951bb0166d5997c0c005565e175ad59e
soubhikdas <d.soubhik@hotmail.com>
2023-11-17 16:59:43 +0530

    Update script.js

--- qrcontent/script.js
+++ qrcontent/script.js
@@ -1,7 +1,7 @@
  'use strict';
 
   if (window.location !== window.parent.location) {
-  var allowedDomains = ['mastrowall.com', 'mastrowall.in','google.com','googleusercontent.com'];
+  var allowedDomains = ['mastrowall.com', 'mastrowall.in'];
   var isAllowedDomain = allowedDomains.some(function(domain) {
       return document.referrer.includes(domain);
   });
