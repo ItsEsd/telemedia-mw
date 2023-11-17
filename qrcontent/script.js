6db757809bb93ec608a0f71c37a889dcff0edc25
soubhikdas <d.soubhik@hotmail.com>
2023-11-17 18:00:49 +0530

    Update script.js

--- qrcontent/script.js
+++ qrcontent/script.js
@@ -24,7 +24,9 @@
         flexItem.innerHTML = `
           <div onclick="openModal('${item.image}', '${item.caption}', '${item.date}', '${item.details}')">
             <div class="flex-imgcon"><img src="${item.image}" alt="${item.caption}" class="img-fluid"></div>
-            <div class="caption">${item.caption}<br><a href="#" class="link">Know More &#9205;</a></div>
+            <div class="caption">${item.caption}<br><p class="link">Know More <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+            <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+          </svg></p></div>
           </div>
         `;
       
@@ -61,5 +63,11 @@
     }
   });
 
-
- 
+  document.getElementById('btncon').addEventListener('click', function(event) {
+      if (event.target.closest('.qrcbtnright img')) {
+          document.getElementById('rgtscrl').click();
+      }
+      else if (event.target.closest('.qrcbtnleft img')){
+        document.getElementById('lftscrl').click();
+      }
+  });
